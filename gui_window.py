af177d7a63aa8fed0f5f29b73b697ce77f098fd5
Light <arikhancis@gmail.com>
2025-07-11 20:09:02 +0300

    Update gui_window.py to explain why it's empty

---
+++ gui_window.py
@@ -0,0 +1,2 @@
+# For now this file is empty because I had hard time importing and giving information to different files about
+# who's when so I took the code to the AI_voice.py to not waste my time that I was able to focus
